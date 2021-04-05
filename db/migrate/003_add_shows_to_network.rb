bfad3996333965aca27e221c4634e17006aab0bd
wei chen <weichen9083@gmail.com>
2021-04-05 04:07:38 +0000

    Done.

--- db/migrate/003_add_shows_to_network.rb
+++ db/migrate/003_add_shows_to_network.rb
@@ -1,8 +1,11 @@
-class AddShowsToNetwork < ActiveRecord::Migration[5.1]
+class AddShowsToNetwork < ActiveRecord::Migration[5.2]
   #to make an additive change to a schema we create
   #a new migration, and then in the change method,
   #we make the change.
   def change
     add_column :shows, :network_id, :integer
+    add_column :shows, :day, :string
+    add_column :shows, :genre, :string 
+    add_column :shows, :season , :string 
   end
 end
