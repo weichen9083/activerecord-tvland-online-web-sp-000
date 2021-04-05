376e56b2322fb962aedcfc23d256027cc6d59760
wei chen <weichen9083@gmail.com>
2021-04-05 04:06:46 +0000

    Automatically backed up by Learn

--- db/migrate/ 007_add_day_and_genre_and_season_to_shows.rb
+++ db/migrate/ 007_add_day_and_genre_and_season_to_shows.rb
@@ -1,8 +1,1 @@
 
-class AddDayAndGenreAndSeasonToShows < ActiveRecord::Migration[5.2]
-  def change 
-    add_column :shows, :day, :string
-    add_column :shows, :genre, :string 
-    add_column :shows, :season , :string 
-  end 
-end 
